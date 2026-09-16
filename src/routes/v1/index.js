454f7a546f234aaeb824046cead5e2998492cbf7
karloz-1 <faxingberlin08@gmail.com>
2026-09-16 17:08:44 -0500

    scaffold de router y endpoints básicos.

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -3,7 +3,9 @@ const router = express.Router();
 
 // Importar rutas especificas
 const usersRoutes = require("./users.routes");
+const workoutsRoutes = require("./workouts.routes");
 
 // Configurar las rutas
 router.use("/users", usersRoutes);
+router.use("/workouts", workoutsRoutes);
 module.exports = router;
