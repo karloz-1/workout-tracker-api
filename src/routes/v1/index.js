ecf95afe3da1bba6ce387a142a9b7e1b3176010d
karloz-1 <faxingberlin08@gmail.com>
2026-09-22 17:25:19 -0500

    feat: nuevo endpoint [exercises]

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -4,8 +4,10 @@ const router = express.Router();
 // Importar rutas especificas
 const usersRoutes = require("./users.routes");
 const workoutsRoutes = require("./workouts.routes");
+const exercisesRoutes = require("./exercises.routes");
 
 // Configurar las rutas
 router.use("/users", usersRoutes);
 router.use("/workouts", workoutsRoutes);
+router.use("/exercises", exercisesRoutes);
 module.exports = router;
